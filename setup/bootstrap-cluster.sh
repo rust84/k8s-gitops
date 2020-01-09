11ea2bf8ca802f1cc94480ed06f6cc3c462dc64a
Russell Hallk <owner@russellhall.co.uk>
2020-01-09 22:30:16 +0000

    convert remaining pi nodes

--- setup/bootstrap-cluster.sh
+++ setup/bootstrap-cluster.sh
@@ -3,7 +3,7 @@
 # nodes
 K3S_MASTER="k3s-0"
 K3S_WORKERS_AMD64="k3s-1 k3s-2"
-K3S_WORKERS_RPI="k3s-5 k3s-6 pi4-c"
+K3S_WORKERS_RPI="pi4-a pi4-b pi4-c"
 K3S_VERSION="v1.17.0+k3s.1"
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
