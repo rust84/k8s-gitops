1957201f57b5a655a97ee98413b4b681aac3f317
Russell Hall <owner@russellhall.co.uk>
2020-07-16 00:13:00 +0100

    use boltdb shipper

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -200,6 +200,7 @@ loadSecretsToVault() {
   kvault "default/powerdns/powerdns-mariadb-helm-values.txt"
   kvault "default/powerdns/powerdns-admin-helm-values.txt"
   kvault "default/powerdns/powerdns-helm-values.txt"
+  kvault "logs/loki/loki-helm-values.txt"
 }
 
 FIRST_RUN=1
