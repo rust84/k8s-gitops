f48601af74e24f82ff33ee9ca3885966bd564166
Russell Hall <owner@russellhall.co.uk>
2021-10-10 00:46:51 +0100

    make frigate external

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -165,7 +165,7 @@ EOF
 loadSecretsToVault() {
   message "writing secrets to vault"
   vault kv put secrets/cert-manager/cloudflare-api-key api-key="$CF_API_KEY"
-  vault kv put secrets/networking/external-dns/cloudflare-api-key cloudflare_api_token="$CF_API_KEY"
+  vault kv put secrets/networking/external-dns/cloudflare-api-key cloudflare_api_token="$EXTERNAL_DNS_CF_API_TOKEN"
 
   ####################
   # helm chart values
